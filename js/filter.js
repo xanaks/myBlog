bf1da270467560643974feaac5efc43ee3097e7a
xanaks <36263837+xanaks@users.noreply.github.com>
2020-10-15 13:04:24 +0400

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,21 +1,13 @@
 let articles = document.querySelectorAll('.item-larticle');
 let filter = document.querySelector('.title-button');
-let bigArticle = document.querySelector('.big-list');
 
 filter.onchange = function () {
   for (let article of articles) {
     if (article.dataset.category !== filter.value && filter.value !== 'all') {
       article.classList.add('hidden');
-     
-    } 
-    else {
+    } else {
       article.classList.remove('hidden');
     }
-     /*  if (article.dataset.category == 'book' || article.dataset.category == 'travel' || article.dataset.category == 'city') {
-      bigArticle.classList.add('hidden');
-  }
-     else {
-      bigArticle.classList.remove('hidden');
-    }  */
   }
 };
+
