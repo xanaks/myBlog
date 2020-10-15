4422ce439445c9efa930515c08641f68de3aba97
xanaks <36263837+xanaks@users.noreply.github.com>
2020-10-15 12:57:58 +0400

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,20 +1,21 @@
 let articles = document.querySelectorAll('.item-larticle');
 let filter = document.querySelector('.title-button');
-let bigArticle = document.querySelector(".big-list");
+let bigArticle = document.querySelector('.big-list');
 
 filter.onchange = function () {
   for (let article of articles) {
     if (article.dataset.category !== filter.value && filter.value !== 'all') {
       article.classList.add('hidden');
-        if (article.dataset.category == "book" || article.dataset.category == "travel" || article.dataset.category == "city") {
+     
+    } 
+    else {
+      article.classList.remove('hidden');
+    }
+       if (article.dataset.category == 'book' || article.dataset.category == 'travel' || article.dataset.category == 'city') {
       bigArticle.classList.add('hidden');
   }
      else {
       bigArticle.classList.remove('hidden');
     }
-    } 
-    else {
-      article.classList.remove('hidden');
-    }
-  
+  }
 };
